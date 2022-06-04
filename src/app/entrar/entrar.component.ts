2a77b159257b42a98b290795c1e3043c645b5a59
Laura Araújo <laulaav@gmail.com>
2022-06-03 21:28:11 -0300

    Conceito de consumo de API login e cadastro

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,4 +1,9 @@
+import { environment } from './../../environments/environment.prod';
+import { AuthService } from '../service/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLogin } from '../model/UserLogin';
+
 
 @Component({
   selector: 'app-entrar',
@@ -6,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entrar.component.css']
 })
 export class EntrarComponent implements OnInit {
+  userLogin: UserLogin = new UserLogin()
+  
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(){
+    window.scroll(0,0)
   }
 
-}
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe({
+      next: (resp: UserLogin) => {
+        this.userLogin = resp
+        environment.nome = this.userLogin.nome;
+        environment.id = this.userLogin.id;
+        environment.usuario = this.userLogin.usuario;
+        environment.foto = this.userLogin.foto;
+        environment.token = this.userLogin.token;
+        environment.tipo = this.userLogin.tipo;
+
+        this.router.navigate(['/inicio'])
+      },
+      error: erro => {
+        if (erro.status == 401) {
+          alert('Usuario ou senha inválidos')
+        }
+      }
+    })
+  }
+    
+  }
+
+
