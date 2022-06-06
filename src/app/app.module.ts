db6268908c0baa59f14c99bc9b09dc4d8a03f452
Laura Araújo <laulaav@gmail.com>
2022-06-06 14:54:00 -0300

    Ajeitando o cadastro e o login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
-import { InicioComponent } from './inicio/inicio.component';
 
 
 
